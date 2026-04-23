822ddcac758012278f0d3e78f028d6266000c134
Milieth <pravda.mahyun@gmail.com>
2026-04-23 12:50:06 +0700

    deploy github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,9 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  site: 'https://Milieth.github.io',
+  base: 'gamedev-portofolio',
   vite: {
-    plugins: [tailwindcss()],
+    plugins: [tailwindcss()]
   },
 });
